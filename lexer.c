542973e0e0170a16e35dbf9d8ca447e6ba0f6098
jrimpila <jrimpila@student.hive.fi>
2025-02-18 14:40:30 +0200

    Work so far.

---
+++ lexer.c
@@ -0,0 +1,225 @@
+#include "minishell.h"
+
+#define RED "\x1b[31m"
+#define RESET "\x1b[0m"
+#define BLUE "\x1b[34m"
+#define GREEN "\x1b[32m"
+
+/*
+
+Step 1: Read
+Input: find . -name "*.txt" | grep "pattern" > results.txt
+
+Step 2: Process Quotes
+Input: find . -name "*.txt" | grep "pattern" > results.txt
+
+Step 3: Split into Commands
+Command 1: find . -name "*.txt" Command 2: grep "pattern" > results.txt
+
+Step 4: Special Operators
+Command 1: find . -name "*.txt" Command 2: grep "pattern" Redirection: > results.txt
+
+Step 5: Expansion
+Command 1: find . -name "*.txt" Command 2: grep "pattern" Redirection: > results.txt
+
+Step 6: Word Splitting
+Command 1 Name: find Command 1 Arguments: . -name "*.txt" Command 2 Name: grep Command 2 Arguments: "pattern"
+
+Step 7: Execution
+Execute Command 1: find . -name "*.txt" Pipe to Command 2: grep "pattern" Redirection: > results.txt
+
+*/
+
+void remove_quotes(t_char *dst, char *src, t_data *data)
+{
+    int i;
+    int in_s_quotes;
+    int in_d_quotes;
+	int k;
+	int exp;
+
+	exp = 0;
+    in_s_quotes = 0;
+    in_d_quotes = 0;
+    i = 0;
+	k = 0;
+    while (src && src[i] != 0)
+    {
+        if (in_s_quotes && src[i] == '\'')
+            in_s_quotes = 0;
+        else if (in_d_quotes && src[i] == '\"')
+            {
+				in_d_quotes = 0;
+				exp = 0;
+				dst[k + 1].blok = 1;
+			}
+        else if (!in_d_quotes && !in_s_quotes && src[i] == '\"')
+            in_d_quotes = 1;
+        else if (!in_d_quotes && !in_s_quotes && src[i] == '\'')
+            {
+				in_s_quotes = 1;
+				dst[k + 1].blok = 1;
+			}
+        else if (in_s_quotes)
+        {
+            dst[k].c = src[i];
+            dst[k].esc = 1;
+			k++;
+        }
+		else if (in_d_quotes)
+        {
+            dst[k].c = src[i];
+			if (src[i] == '$')
+            	exp = 1;
+			else if (exp == 1)
+			{
+				if (ft_isalnum(src[i]) == 0 && src[i] != '_')
+					{
+						exp = 0;
+						dst[k + 1].blok = 1;
+					}
+			}
+			if (exp == 0)		
+				dst[k].esc = 1;
+			k++;
+        }
+        else
+        {
+			dst[k].c = src[i];
+			k++;
+		}
+        i++;
+    }
+    dst[i].c = '\0'; // Null-terminate the dst array
+}
+
+// real << should not have a space on the right side, I will have to check what is the correct behavior
+//It seems to require non whitespace character 
+
+//Should get caught if it is surrounded by spaces but give error
+void mark_commands(t_char *com_line, t_data *data)
+{
+    int i = 0;
+    while(com_line[i].c != 0)
+    {
+        if (i == 0 || com_line[i].esc || com_line[i - 1].esc || com_line[i + 1].c == 0 || com_line[i + 1].esc)
+            ;
+        else if (com_line[i - 1].c == '<' && !com_line[i - 2].esc && com_line[i - 2].c == ' ' && com_line[i].c == '<' && com_line[i + 1].c == ' ')
+            com_line[i].com = 1;
+        else if (com_line[i - 1].c == '>' && !com_line[i - 2].esc && com_line[i - 2].c == ' ' && com_line[i].c == '>' && com_line[i + 1].c == ' ')
+            com_line[i].com = 1;
+        else if (com_line[i - 1].c == ' ' && com_line[i].c == '>' && com_line[i + 1].c == '>' && !com_line[i + 2].esc && com_line[i + 2].c == ' ')
+            com_line[i].com = 1;
+        else if (com_line[i].c == '<' && com_line[i + 1].c == '<' && com_line[i + 2].c != 0 && (com_line[i + 2].esc == 0 && com_line[i + 2].c != ' '))
+            com_line[i].com = 1;
+        else if (com_line[i - 1].c != ' ' || com_line[i + 1].c != ' ')
+            ;
+        else if (com_line[i].c == '|' || com_line[i].c == '<' || com_line[i].c == '>')
+            com_line[i].com = 1;
+        i++;
+    }
+}
+
+void mark_env_var(t_char *newline, int start, t_data *data)
+{
+	int end;
+
+	end = start;
+	newline[end].var = 1;
+	end++;
+	if (newline[end].c == 0 || newline[end].esc ||
+	(!ft_isalpha(newline[end].c) && newline[end].c != '_')) 
+		return ; 
+	else 
+	{
+		newline[end].var = 1;
+		end++;
+	}
+	while(newline[end].c != 0 && !newline[end].esc && (ft_isalnum(newline[end].c) || newline[end].c == '_' ))
+	{
+		if (newline[end + 1].blok != 1)
+			newline[end].var = 1;
+		end++;
+	}
+
+}
+
+void	expand_arguments(t_char *newline, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (newline[i].c != 0)
+	{
+		if (newline[i].esc)
+			;
+		else if (newline[i].c == '$')
+			mark_env_var(newline, i, data);
+		i++;
+	}
+}
+
+
+void lexify(char *line, t_data *data)
+{
+    t_char *newline;
+    int i;
+
+    newline = ft_calloc(ft_strlen(line) + 1, sizeof(t_char), data);
+    remove_quotes(newline, line, data);
+	mark_commands(newline, data);
+	expand_arguments(newline, data);
+    i = 0;
+    while (newline[i].c)
+    {
+		if (newline[i].esc && newline[i].c == ' ')
+			printf("%s_%s", RED, RESET);
+        else if (newline[i].esc)
+        {
+            printf("%s%c%s", RED, newline[i].c, RESET);
+        }
+		else if (newline[i].com)
+		{
+            printf("%s%c%s", BLUE, newline[i].c, RESET);
+        }
+		else if (newline[i].var)
+		{
+            printf("%s%c%s", GREEN, newline[i].c, RESET);
+        }
+        else
+            printf("%c", newline[i].c);
+        i++;
+    }
+    printf("\n");
+   
+}
+
+int main(int argc, char *argv[])
+{
+    static t_data data[1]; 
+    // Example lines for testing your parsing function
+char *test_lines[] =
+{
+    "echo $HOME > home_dir.txt",
+    "cp $PATH/path/to/file $HOME/backup/",
+    "ls -la $USER > user_list.txt",
+    "find $HOME -type f -name '*.log' | xargs grep 'er\"r\"$or' > error_logs.txt",
+    "ps -ef | grep $SHELL > shell_processes.txt",
+    "cd $TEMP && ls -l > temp_dir_list.txt",
+    "tar -czvf $BACKUP/archive.tar.gz $HOME/documents",
+    "echo tadaa \"$HOSTNAM'E'\" << hostname.txt",
+    "awk '{print $2}' $DATA_FILE > second_column.txt",
+    "df -h $HOME > disk_usage_home.txt",
+	"echo '$HOME'",
+	"./pipex infile \"ls -a\" \"cat -e\" outfile | \"cat -e\" outfile"
+};
+
+    
+    for (int i = 0; i < sizeof(test_lines) / sizeof(test_lines[0]); i++)
+	{
+        printf("Test line %d: %s\n", i + 1, test_lines[i]);
+        lexify(test_lines[i], data);
+        printf("\n");
+    }
+    return (0);
+}
--- minishell.h
+++ minishell.h
@@ -5,28 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrimpila <jrimpila@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/26 16:50:22 by jrimpila          #+#    #+#             */
-/*   Updated: 2025/02/16 18:31:37 by jrimpila         ###   ########.fr       */
+/*   Created: 2025/02/17 10:39:37 by jrimpila          #+#    #+#             */
+/*   Updated: 2025/02/18 12:16:33 by jrimpila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <stddef.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <stdio.h>
 # include <limits.h>
 # include <unistd.h>
-#include <errno.h>
-#include <stdbool.h> 
+# include <errno.h>
+# include <stdbool.h> 
 
 #define ENV_SIZE 4096
 
 
-
+//ghost variable marks removed quotes that only act as delimiter for $expansion
 typedef struct s_char
 {
     char c;        
-    bool escaped;  
+    bool esc;
+	bool com;
+	bool var;
+	bool blok;
 } t_char;
 
 
@@ -45,9 +48,9 @@ typedef struct s_node
 {
 	struct s_node	*prev;
 	struct s_node	*next;
-	char *str;
-	char *original;
-	t_token type;
+	char			*str;
+	char			*original;
+	t_token			type;
 }	t_node;
 
 typedef struct s_stack
@@ -58,12 +61,12 @@ typedef struct s_stack
 
 typedef struct s_data
 {
-	int duh;
-	char **history;
-	int his_i;
-	char **env;
+	int		duh;
+	char	**history;
+	int		his_i;
+	char	**env;
 	t_stack tokens;
-} t_data;
+}	t_data;
 
 
 typedef struct s_point
@@ -73,26 +76,9 @@ typedef struct s_point
 	int		end;
 }	t_point;
 
-
-char	*ft_strdup(const char *s);
+void ft_exit(t_data *data, char *message, int exit_code);
 void	*ft_calloc(size_t nmemb, size_t size, t_data *data);
-void	ft_exit(t_data *data, char *errormessage, int exit_code);
-void	*ft_calloc(size_t nmemb, size_t size, t_data *data);
-void	*ft_memset(void *s, int c, size_t n);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *big, const char *little, size_t len);
 size_t	ft_strlen(const char *s);
-int find_env(const char *str, t_data *data);
-char	**ft_split(char const *s, char c);
-char	*ft_strjoin(char const *s1, char const *s2);
-int export(t_data *data, char **tokens);
-char	*ft_strchr(const char *s, int c);
-void	ft_lstadd_back(t_stack *stack, t_node *new);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-t_node *create_node(char *setstr, t_token set_type, t_data *data);
+void	*ft_memset(void *s, int c, size_t n);
 int		ft_isalpha(int c);
-int		ft_isalnum(int c);
-char *ft_strndup(const char *s, size_t start, size_t end);
-void add_behind(t_node *target, t_node *new, t_data *data);
-char *get_own_env(t_data *data, char *str);
-int	tokenize(t_data *data, const char *startp, int length, t_token t_type);
+int	ft_isalnum(int c);
