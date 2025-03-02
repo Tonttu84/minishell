cbc2d58ae5149bcd82c62353462bad8e400e28e0
jrimpila <jrimpila@student.hive.fi>
2025-03-02 13:30:57 +0200

    minor fix

--- srcs/test.c
+++ srcs/test.c
@@ -6,7 +6,7 @@
 /*   By: jrimpila <jrimpila@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 11:23:16 by jrimpila          #+#    #+#             */
-/*   Updated: 2025/03/01 20:28:53 by jrimpila         ###   ########.fr       */
+/*   Updated: 2025/03/02 13:17:41 by jrimpila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ void	test(void)
 	// Example lines for testing your parsing function
 	char *test_lines[] =
 	{
+		
 		//"ls -la | cat > out.txt <test.c | echo $blaa?"
-		"cat \"$HOME/i\"\"nput.txt\">>outfile.txt \'\' \'\'\"\" | echo $US\"\"ER \'\'grep $invalid \"search_pattern\" < \"$HOME/input.txt\" <<EOF > \"$HOME/error.log\" | sort | uniq |   echo > test.txt \"Found line: \"$line\"\" | echo \"Current user: $USER\" | echo \"Non-existent variable: $NONEXISTENTVAR\"",
+		"cat \"$HOME/i\"\"nput.txt\">>outfile.txt \'\' \'\'\"\" | echo $US\"\"ER \'\'grep $invalid \"search_pattern\" < \"$HOME/input.txt\" <<EOF > \"$HOME/error.log\" | sort | uniq |   echo > test.txt \"Found line: \"$line\"\" | echo \"Current user: $USER\" | echo \"Non-existent variable: $NONEXISTENTVAR\" ",
 	};
 	for (i = 0; (unsigned int)i < sizeof(test_lines)
 		/ sizeof(test_lines[0]); i++)
