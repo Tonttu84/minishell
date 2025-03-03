5c07cc8ef9139f2470750babb8c90d62d131d2dd
Joonas Tuomi <jtuomi@student.hive.fi>
2025-03-03 20:04:44 +0200

    WIP: seeking the path, checking x rights with access, checking whether it is in env and if not found replacing executable with /usr/lib/command-not-found <cmd>.

---
+++ srcs/path.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtuomi <jtuomi@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 19:21:10 by jtuomi            #+#    #+#             */
+/*   Updated: 2025/03/03 20:03:16 by jtuomi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+static char	*is_path_in_env(void)
+{
+    t_data *data;
+    int i;
+    char *s1;
+
+    data = get_data();
+    i = 0;
+    while (data->env[i])
+	{
+		s1 = ft_strnstr(data->env[i], "PATH=", 5);
+		if (s1)
+			return (&data->env[i][5]);
+		i++;
+	}
+	return (NULL);
+}
+
+/*
+** checks whether command can be found and if it can't replaced the command
+** later to be executed by /usr/lib/command-not-found to offer suggestions
+** how to install or fix a typo etc.
+*/
+static void	command_not_found(int nbr)
+{
+    t_data *data;
+    char *s;
+
+    data = get_data();
+    s = "/usr/lib/command-not-found";
+    ft_memmove(data->page[nbr]->array[1], data->page[nbr]->array[0], \
+               ft_strlen(data->page[nbr]->array[1]));
+	ft_memmove(data->page[nbr]->array[1], s, ft_strlen(s) + 1);
+}
+
+/*
+** this seeks path, check what kind of command we're dealing with and
+** calls the other functions accordingly.
+*/
+void	util_parse_args(t_data *data, int i, void *ptr, bool flag)
+{
+	if (data->env[0])
+	{
+		ptr = is_path_in_env();
+		if (!ptr)
+			flag = false;
+	}
+	while (data->page[i]->array[0])
+	{
+        if (ft_strnstr(data->page[i]->array[0], "/", 100)
+			 ;
+        else if (command_in_path(pipex, i, NULL, 0))
+			command_not_found(pipex, i);
+		i++;
+	}
+}
+
+/*
+** this join / to end of every file in path and that to command
+** then checks with access if file exists and tries to see
+** whether that file is also executable (has those rights).
+*/
+bool	command_in_path(char *path, int nbr, char *cmd_p, int i)
+{
+	char	*tmp;
+
+	while (path)
+	{
+
+	}
+		return (false);
+	return (true);
+}
