b7ca1f34d813e7ed9c25042af7773b53d10af2bf
jrimpila <jrimpila@student.hive.fi>
2025-03-10 13:03:28 +0200

    The solution didnt work after all, undoing

--- srcs/input.c
+++ srcs/input.c
@@ -6,7 +6,7 @@
 /*   By: jrimpila <jrimpila@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 11:11:31 by jrimpila          #+#    #+#             */
-/*   Updated: 2025/03/10 12:44:44 by jrimpila         ###   ########.fr       */
+/*   Updated: 2025/03/10 13:02:55 by jrimpila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,8 +59,6 @@ void	prompt_input(void)
 		process(line);
 		util_parse_args(get_data(), 0);
 		execute(get_data()->page[0], pfd, 1, 0);
-		usleep(1);
-		write(1, "\n", 1);
 	}
 }
 
