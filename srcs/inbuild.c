891ca327cabcb374427172d1516ab68539e22d40
jrimpila <jrimpila@student.hive.fi>
2025-03-11 16:54:46 +0200

    changed how builtins could be handled

--- srcs/inbuild.c
+++ srcs/inbuild.c
@@ -6,41 +6,35 @@
 /*   By: jrimpila <jrimpila@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 12:05:36 by jrimpila          #+#    #+#             */
-/*   Updated: 2025/03/05 12:32:10 by jrimpila         ###   ########.fr       */
+/*   Updated: 2025/03/11 16:49:16 by jrimpila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	test_inbuilds(t_node *cmd)
+int run_builtin(int argc, char *argv[], char *env[])
 {
-	if (cmd->str[0].c == 'c' && cmd->str[1].c == 'd' && cmd->str[2].c == 0
-		&& cmd->str[2].esc == 0)
+	//Not sure if I can take input like this to function withot counting variables?
+	if (argc == 0)
 		return (1);
-	else if (cmd->str[0].c == 'e' && cmd->str[1].c == 'c'
-		&& cmd->str[2].c == 'h' && cmd->str[1].c == 'o' && cmd->str[2].c == 0
-		&& cmd->str[2].esc == 0)
-		return (2);
-	else if (cmd->str[0].c == 'e' && cmd->str[1].c == 'n'
-		&& cmd->str[2].c == 'v' && cmd->str[3].c == 0 && cmd->str[3].esc == 0)
-		return (3);
-	else if (cmd->str[0].c == 'e' && cmd->str[1].c == 'x'
-		&& cmd->str[2].c == 'o' && cmd->str[3].c == 'r' && cmd->str[5].c == 't'
-		&& cmd->str[6].c == 0 && cmd->str[6].esc == 0)
-		return (4);
-	else if (cmd->str[0].c == 'p' && cmd->str[1].c == 'w'
-		&& cmd->str[2].c == 'd' && cmd->str[3].c == 0 && cmd->str[3].esc == 0)
-		return (5);
-	else if (cmd->str[0].c == 'u' && cmd->str[1].c == 'n'
-		&& cmd->str[2].c == 's' && cmd->str[3].c == 'e' && cmd->str[4].c == 't'
-		&& cmd->str[5].c == 0 && cmd->str[5].esc == 0)
-		return (6);
-	else if (cmd->str[0].c == 'e' && cmd->str[1].c == 'x'
-		&& cmd->str[2].c == 'i' && cmd->str[3].c == 't' && cmd->str[4].c == 0
-		&& cmd->str[4].esc == 0)
-		return (7);
-	else
+	if (ft_strncmp("cd", argv[0], 3 == 0 ))
+			cd(argc, argv, env);
+	else if (ft_strncmp("pwd", argv[0], 4 == 0 ))
+		pwd(argc, argv, env);
+	else if (ft_strncmp("echo", argv[0], 5 == 0 ))
+		echo(argc, argv, env);
+	else if (ft_strncmp("env", argv[0], 4 == 0 ))
+		env(argc, argv, env);
+	else if (ft_strncmp("export", argv[0], 7 == 0 ))
+		export(argc, argv, env);
+	else if (ft_strncmp("unset", argv[0], 6 == 0 ))
+		unset(argc, argv, env);
+	else if (ft_strncmp("exit", argv[0], 5 == 0 ))
+		exit(argc, argv, env);
+	else if
 		return (0);
+	return (1);
+		
 }
 
 int	pwd(void)
